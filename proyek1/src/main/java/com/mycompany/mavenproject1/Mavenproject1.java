98314f4c2383663946f78cf6ca623b61c0b6c7b2
figuran04 <dikaelsaputra2406@gmail.com>
2024-08-27 11:22:57 +0700

    add: proyek1

---
+++ proyek1/src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author User
+ */
+public class Mavenproject1 {
+
+    public static void main(String[] args) {
+        System.out.println("Nama : Dika Elsaputra");
+        System.out.println("NPM : 2320506032");
+        System.out.println("Alamat : Magelang");
+        System.out.println("No_Hp : 081234567890");
+    }
+}
